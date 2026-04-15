99272ec6c64b3e8cbd2d3447a385354e63496446
Satyam Malik <satyammalik414@gmail.com>
2026-04-15 16:13:52 +0530

    UC11: OOP-based palindrome checker using service class

---
+++ UC11PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UC11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome?: " + result);
+    }
+}
